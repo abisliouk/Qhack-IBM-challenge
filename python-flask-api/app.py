1dc01b81afe82ecb3832f8398d428cee9190700a
Ludwig Fügner Laptop <lfuegner@gevb.de>
2024-04-03 23:45:39 +0200

    build: flask POST-endpoint

--- python-flask-api/app.py
+++ python-flask-api/app.py
@@ -2,12 +2,15 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-# Route to add a new book
 @app.route('/generate-onboarding', methods=['POST'])
 def add_book():
     parameters = request.json
-    # Here comes the rag and rest of the backend
-    return jsonify("Marc ist ein gut bestückter 🍆 und aussehender Mann!!!😍"), 201
+    companyResources = parameters["companyResources"]
+    employeeResources = parameters["employeeResources"]
+    requirements = parameters["requirements"]
+
+
+    return jsonify(companyResources,employeeResources,requirements), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
--- python-flask-api/post.py
+++ python-flask-api/post.py
@@ -3,15 +3,24 @@ import requests
 # Define the base URL of your Flask API
 base_url = 'http://127.0.0.1:5000/'
 
-# Data for the new book
-new_onboarding = {"a": "10", "b": "10"}
+new_onboarding = {
+  "companyResources": [
+    "https://www.uni-mannheim.de/studium/im-studium/pruefungen/pruefung-ablegen/pruefungstermine/",
+    "https://www.uni-mannheim.de/it/support/speedtest/"
+  ],
+  "employeeResources": ["https://www.uni-mannheim.de/it/support/speedtest/"],
+  "requirements": [
+    "Include a showcase of employees",
+    "Make sure web skills like HTML, CSS and Javascript are covered"
+  ]
+}
 
-# Make a POST request to add a new book
+# Make a POST request to generate the onboarding
 response = requests.post(f'{base_url}/generate-onboarding', json=new_onboarding)
 
 # Check if the request was successful
 if response.status_code == 201:
-    print("New Onboarding added successfully:")
+    print("New Onboarding raged successfully:")
     print(response.json())
 else:
     print(f"Error: {response.status_code}")
